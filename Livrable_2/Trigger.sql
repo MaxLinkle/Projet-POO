f79677ceee82103bb0bd09ce5a6c0505473f275c
atarte <tarte.antoine@gmail.com>
2020-11-20 16:12:26 +0100

    update Trigger

--- Livrable_2/Trigger.sql
+++ Livrable_2/Trigger.sql
@@ -1,13 +1,18 @@
 DELIMITER |
-DROP TRIGGER IF EXISTS delete_Personnel;
+DROP TRIGGER IF EXISTS delete_Personnel |
 CREATE TRIGGER delete_Personnel BEFORE DELETE
 ON Personnel FOR EACH ROW
 BEGIN
   DELETE FROM Adresse_personnel WHERE (ID_personnel = OLD.ID_personnel);
+
+  IF ID_superieur = ID_personnel THEN
+    UPDATE Personnel
+    SET ID_superieur = NULL;
+  END IF;
 END |
 
 
-DROP TRIGGER IF EXISTS delete_Client;
+DROP TRIGGER IF EXISTS delete_Client |
 CREATE TRIGGER delete_Client BEFORE DELETE
 ON Client FOR EACH ROW
 BEGIN
@@ -15,23 +20,23 @@ BEGIN
 END |
 
 
-DROP TRIGGER IF EXISTS delete_Commande;
+DROP TRIGGER IF EXISTS delete_Commande |
 CREATE TRIGGER delete_Commande BEFORE DELETE
 ON Commande FOR EACH ROW
 BEGIN
-  DELETE FROM Fournir WHERE (ID_commande = OLD.commande);
+  DELETE FROM Fournir WHERE (ID_commande = OLD.ID_commande);
 END |
 
+DELIMITER ;
 
-DROP TRIGGER IF EXISTS update_Catalogue;
-CREATE TRIGGER update_Catalogue AFTER UPDATE
-ON Catalogue FOR EACH ROW
-BEGIN
-  DROP VIEW IF EXISTS Catalogue_actif;
-
-  CREATE VIEW Catalogue_actif AS
-  SELECT ID_catalogue_actif, nom, reference, prix_ht, taux_tva, stock, seuil_reapprovisionnement
-  FROM Catalogue
-  WHERE actif = true;
-END |
-DELIMITER;
+-- DROP TRIGGER IF EXISTS update_Catalogue |
+-- CREATE TRIGGER update_Catalogue AFTER UPDATE
+-- ON Catalogue FOR EACH ROW
+-- BEGIN
+--   DROP VIEW IF EXISTS Catalogue_actif;
+--
+--   CREATE VIEW Catalogue_actif (ID_catalogue_actif, nom, reference, prix_ht, taux_tva, stock, seuil_reapprovisionnement) AS
+--   SELECT ID_catalogue_actif, nom, reference, prix_ht, taux_tva, stock, seuil_reapprovisionnement
+--   FROM Catalogue
+--   WHERE actif = true;
+-- END |
