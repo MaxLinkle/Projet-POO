821cae11d81333c22af22e7e839ac507a7a1ae53
C-atarina <catarina.silvaroriz@viacesi.fr>
2020-11-25 17:42:50 +0100

    MyForm .h 25/11/2020 17h42

--- Livrable_2/catarina/MyForm.h
+++ Livrable_2/catarina/MyForm.h
@@ -58,15 +58,26 @@ namespace Catalogue {
 				std::cout << "Success!\n";
 			}
 			
-			qstate = mysql_query(con, "SELECT nom, prix_ht FROM Catalogue");
+			CheckBox^ Nom = gcnew CheckBox();
+			qstate = mysql_query(con, "SELECT nom, prix_ht, stock, taux_tva FROM Catalogue");
 			
 			if (!qstate)
 			{
 				res = mysql_store_result(con);
 				while (row = mysql_fetch_row(res))
 				{
-					checkedListBox1->Items->Add(gcnew String(row[0]) + "         " + gcnew String(row[1]));
+					int n = dataGridView1->Rows->Add();
+					double ht = atof(row[1]);
+					double tva = atof(row[3]);
+					double ttc = ht * ((tva/100)+1);
+					
+					dataGridView1->Rows[n]->Cells[1]->Value = gcnew String(row[0]);
+					dataGridView1->Rows[n]->Cells[2]->Value = ttc;
+					dataGridView1->Rows[n]->Cells[4]->Value = gcnew String(row[2]);
+					dataGridView1->Rows[n]->Cells[3]->Value = 0;
+					
 				}
+
 			}
 		}
 
@@ -81,21 +92,31 @@ namespace Catalogue {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::CheckedListBox^ checkedListBox1;
+
 	private: System::Windows::Forms::Button^ retour;
 
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ acheter;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
 
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::DataGridViewCheckBoxColumn^ Check;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Nom;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Prix;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Quantite;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Max;
+	private: System::Windows::Forms::Label^ label2;
+
+
+	private: System::ComponentModel::IContainer^ components;
 
 	protected:
-
 
 	private:
 		/// <summary>
 		/// Variable nécessaire au concepteur.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -104,20 +125,19 @@ namespace Catalogue {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->checkedListBox1 = (gcnew System::Windows::Forms::CheckedListBox());
 			this->retour = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->acheter = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->Check = (gcnew System::Windows::Forms::DataGridViewCheckBoxColumn());
+			this->Nom = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Prix = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Quantite = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Max = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// checkedListBox1
-			// 
-			this->checkedListBox1->FormattingEnabled = true;
-			this->checkedListBox1->Location = System::Drawing::Point(75, 36);
-			this->checkedListBox1->Name = L"checkedListBox1";
-			this->checkedListBox1->ScrollAlwaysVisible = true;
-			this->checkedListBox1->Size = System::Drawing::Size(450, 364);
-			this->checkedListBox1->TabIndex = 0;
 			// 
 			// retour
 			// 
@@ -139,31 +159,135 @@ namespace Catalogue {
 			// 
 			// acheter
 			// 
-			this->acheter->Location = System::Drawing::Point(450, 416);
+			this->acheter->Location = System::Drawing::Point(593, 467);
 			this->acheter->Name = L"acheter";
 			this->acheter->Size = System::Drawing::Size(75, 23);
 			this->acheter->TabIndex = 3;
 			this->acheter->Text = L"Acheter";
 			this->acheter->UseVisualStyleBackColor = true;
+			this->acheter->Click += gcnew System::EventHandler(this, &MyForm::acheter_Click);
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(5) {
+				this->Check,
+					this->Nom, this->Prix, this->Quantite, this->Max
+			});
+			this->dataGridView1->Location = System::Drawing::Point(75, 37);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(593, 397);
+			this->dataGridView1->TabIndex = 4;
+			this->dataGridView1->CellValidated += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellValidated);
+			// 
+			// Check
+			// 
+			this->Check->HeaderText = L"";
+			this->Check->Name = L"Check";
+			this->Check->Width = 50;
+			// 
+			// Nom
+			// 
+			this->Nom->HeaderText = L"Nom";
+			this->Nom->Name = L"Nom";
+			this->Nom->ReadOnly = true;
+			this->Nom->Resizable = System::Windows::Forms::DataGridViewTriState::True;
+			this->Nom->SortMode = System::Windows::Forms::DataGridViewColumnSortMode::NotSortable;
+			this->Nom->Width = 300;
+			// 
+			// Prix
+			// 
+			this->Prix->HeaderText = L"Prix TTC";
+			this->Prix->Name = L"Prix";
+			this->Prix->ReadOnly = true;
+			// 
+			// Quantite
+			// 
+			this->Quantite->HeaderText = L"Quantite";
+			this->Quantite->Name = L"Quantite";
+			// 
+			// Max
+			// 
+			this->Max->HeaderText = L"Quantite Max";
+			this->Max->Name = L"Max";
+			this->Max->ReadOnly = true;
+			this->Max->Visible = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(376, 483);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(53, 18);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Total : ";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(376, 454);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(146, 18);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Nombre d\'article(s) : ";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(592, 464);
+			this->ClientSize = System::Drawing::Size(736, 527);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->acheter);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->retour);
-			this->Controls->Add(this->checkedListBox1);
+			this->Location = System::Drawing::Point(333, 36);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 		}
 
+	private: System::Void acheter_Click(System::Object^ sender, System::EventArgs^ e) {
+		for (int n = 0; n < dataGridView1->RowCount; n++)
+		{
+			if (Convert::ToInt32(dataGridView1->Rows[n]->Cells[3]->Value) > Convert::ToInt32(dataGridView1->Rows[n]->Cells[4]->Value))
+			{	
+				dataGridView1->Rows[n]->DefaultCellStyle->BackColor = System::Drawing::Color::PaleVioletRed;
+				MessageBox::Show("Quantite Indisponible. \n Quantite Max : " + dataGridView1->Rows[n]->Cells[4]->Value, "Info", MessageBoxButtons::OK, MessageBoxIcon::Error);
+			}
+			else if (Convert::ToInt32(dataGridView1->Rows[n]->Cells[3]->Value) <= Convert::ToInt32(dataGridView1->Rows[n]->Cells[4]->Value))
+			{
+				dataGridView1->Rows[n]->DefaultCellStyle->BackColor = System::Drawing::Color::White;
+			}
+		}
+		
+	}
+private: System::Void dataGridView1_CellValidated(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	double total = 0;
+	double totAr = 0;
+	for (int n = 0; n < dataGridView1->RowCount; n++)
+	{
+		bool isSelected = Convert::ToBoolean(dataGridView1->Rows[n]->Cells["Check"]->Value);
+		if (isSelected == true)
+		{
+			total += (Convert::ToDouble(dataGridView1->Rows[n]->Cells["Prix"]->Value)) * (Convert::ToDouble(dataGridView1->Rows[n]->Cells["Quantite"]->Value));
+			totAr += Convert::ToDouble(dataGridView1->Rows[n]->Cells["Quantite"]->Value);
+		}
+	}
+	label1->Text = "Total : " + total.ToString() + " €";
+	label2->Text = "Nombre d'article(s) : " + totAr.ToString();
+}
 };
 }
