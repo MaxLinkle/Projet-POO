03195244cc669229e080cea960c50035143ced59
atarte <tarte.antoine@gmail.com>
2020-12-01 20:04:58 +0100

    update procedure stat

--- Livrable_2/Procedure_stat.sql
+++ Livrable_2/Procedure_stat.sql
@@ -4,11 +4,6 @@ DELIMITER |
 DROP PROCEDURE IF EXISTS Stat_Plus_vendu |
 CREATE PROCEDURE Stat_Plus_vendu ()
 BEGIN
-  DECLARE EXIT HANDLER FOR SQLEXCEPTION
-  BEGIN
-    ROLLBACK;
-  END;
-
   SELECT Catalogue.nom, SUM(Fournir.quantite) AS quantite_total
   FROM Fournir
   INNER JOIN Catalogue ON Catalogue.ID_article = Fournir.ID_article
@@ -21,11 +16,6 @@ END |
 DROP PROCEDURE IF EXISTS Stat_Moins_vendu |
 CREATE PROCEDURE Stat_Moins_vendu ()
 BEGIN
-  DECLARE EXIT HANDLER FOR SQLEXCEPTION
-  BEGIN
-    ROLLBACK;
-  END;
-
   SELECT Catalogue.nom, SUM(Fournir.quantite) AS quantite_total
   FROM Fournir
   INNER JOIN Catalogue ON Catalogue.ID_article = Fournir.ID_article
@@ -38,7 +28,15 @@ END |
 DROP PROCEDURE IF EXISTS Stat_Achat_stock |
 CREATE PROCEDURE Stat_Achat_stock ()
 BEGIN
-  SELECT Catalogue.nom, Catalogue.stock, AVG(Catalogue.prix_ht * 0.5) AS Valeur_achat
+  SELECT Catalogue.nom, Catalogue.stock, AVG(Catalogue.stock * Catalogue.prix_ht * 0.5) AS Valeur_achat
+  FROM Catalogue;
+END |
+
+
+DROP PROCEDURE IF EXISTS Stat_Valeur_stock |
+CREATE PROCEDURE Stat_Valeur_stock ()
+BEGIN
+  SELECT Catalogue.nom, Catalogue.stock, AVG(Catalogue.stock * Catalogue.prix_ht) AS Valeur_Commercial
   FROM Catalogue;
 END |
 
@@ -46,18 +44,17 @@ END |
 DROP PROCEDURE IF EXISTS `Stat_Panier_moy` |
 CREATE PROCEDURE `Stat_Panier_moy` (IN `PNom` VARCHAR(50),IN `PPrenom` VARCHAR(50))
 BEGIN
-SELECT AVG(prix_ht+prix_tva)
-FROM Client NATURAL JOIN Commande
-WHERE nom = PNom AND prenom = PPrenom;
+  SELECT AVG(prix_ht+prix_tva)
+  FROM Client NATURAL JOIN Commande
+  WHERE nom = PNom AND prenom = PPrenom;
+END |
+
+
+DROP PROCEDURE IF EXISTS `Stat_Total_achat_client` |
+CREATE PROCEDURE `Stat_Total_achat_client` (IN `PNom` VARCHAR(50),IN `PPrenom` VARCHAR(50))
+BEGIN
+  SELECT SUM(prix_ht+prix_tva)
+  FROM Client NATURAL JOIN Commande
+  WHERE nom = PNom AND prenom = PPrenom;
 END |
 DELIMITER ;
-
-DELIMITER |
-DROP PROCEDURE IF EXISTS `Stat_Panier_moy` |
-CREATE PROCEDURE `Stat_Panier_moy` (IN `PNom` VARCHAR(50),IN `PPrenom` VARCHAR(50))
-BEGIN
-SELECT AVG(prix_ht+prix_tva)
-FROM Client NATURAL JOIN Commande 
-WHERE nom = PNom AND prenom = PPrenom;
-END |
-DELIMITER;
