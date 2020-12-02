9fae2e017a48f40d8161d11a5b2ad59bc407baba
atarte <tarte.antoine@gmail.com>
2020-12-02 16:28:59 +0100

    ajout de procedure

--- Livrable_2/antoine/procedure_perso.sql
+++ Livrable_2/antoine/procedure_perso.sql
@@ -147,7 +147,7 @@ BEGIN
 
   END IF;
 END |
--- DELIMITER ;
+
 
 DROP PROCEDURE IF EXISTS verification_Adresse_client |
 CREATE PROCEDURE verification_Adresse_client (IN i_id_client INT)
@@ -225,5 +225,67 @@ BEGIN
   SELECT *
   FROM Catalogue
   WHERE Catalogue.nom = i_nom AND Catalogue.reference = i_ref AND Catalogue.prix_ht = i_prix_ht AND Catalogue.taux_tva = i_tva AND Catalogue.coefficient_economie = i_coef AND Catalogue.stock = i_stock AND Catalogue.seuil_reapprovisionnement = i_seuil AND Catalogue.actif = i_actif;
+END |
+
+-- DELIMITER |
+DROP PROCEDURE IF EXISTS update_Catalogue_stock |
+CREATE PROCEDURE update_Catalogue_stock (IN i_id_art INT, IN i_dif INT)
+BEGIN
+  DECLARE EXIT HANDLER FOR SQLEXCEPTION
+  BEGIN
+    ROLLBACK;
+  END;
+
+  UPDATE Catalogue
+  SET
+    Catalogue.stock = Catalogue.stock - i_dif
+  WHERE Catalogue.ID_article = i_id_art;
+END |
+
+
+DROP PROCEDURE IF EXISTS ajout_Commande |
+CREATE PROCEDURE ajout_Commande(IN i_prix_ht INT, IN i_prix_tva INT, IN i_remise INT, IN date_liv DATE, IN date_emi DATE, IN date_paye DATE, IN date_sol DATE, IN i_id_adresse_de_livraison INT, IN i_id_adresse_facturation INT, IN i_id_cli INT, IN id_paye INT)
+BEGIN
+  DECLARE adresseliv_cli INT;
+  DECLARE adressefact_cli INT;
+  DECLARE paiement_cli INT;
+  DECLARE id_commande INT;
+
+  DECLARE last INT;
+
+  DECLARE EXIT HANDLER FOR SQLEXCEPTION
+  BEGIN
+    ROLLBACK;
+  END;
+
+  SELECT ID_adresse_client INTO adresseliv_cli FROM Adresse_client WHERE adresse_client = i_id_adresse_de_livraison;
+  SELECT ID_adresse_client INTO adressefact_cli FROM Adresse_client WHERE adresse_client = i_id_adresse_facturation;
+  SELECT ID_paiement INTO paiement_cli FROM Paiement WHERE moyen_paiement = id_paye;
+
+  INSERT INTO Commande(ID_commande, reference, prix_ht, prix_tva, remise, date_livraison_prevue, date_emission, date_paiement, date_solde, ID_adresse_livraison, ID_adresse_facturation, ID_client, ID_paiement)
+  VALUES(NULL, "0", i_prix_ht, i_prix_tva, i_remise, date_liv, date_emi, date_paye, date_sol,adresseliv_cli, adressefact_cli, i_id_cli, paiement_cli);
+
+  SELECT LAST_INSERT_ID() INTO id_commande;
+  CALL generer_reference(i_id_cli, id_commande, @reference);
+
+  UPDATE Commande
+  SET reference = @reference
+  WHERE Commande.ID_commande = id_commande;
+
+  SELECT LAST_INSERT_ID() INTO last;
+  SELECT last;
+END |
+
+
+DROP PROCEDURE IF EXISTS ajout_Fournir |
+CREATE PROCEDURE ajout_Fournir (IN i_quant INT, IN i_id_art INT, IN i_id_com INT)
+BEGIN
+  DECLARE EXIT HANDLER FOR SQLEXCEPTION
+  BEGIN
+    ROLLBACK;
+  END;
+
+  INSERT INTO Fournir (ID_fournir, quantite, ID_article, ID_commande)
+  VALUES (NULL, i_quant, i_id_art, i_id_com);
 END |
 DELIMITER ;
